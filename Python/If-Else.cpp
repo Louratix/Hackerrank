ba15abc3d1325da3822e6396df655d4f994d3454
Louratix <edouard30031998@gmail.com>
2022-05-14 18:30:32 +0900

    added code in Python and C++

--- Python/If-Else.cpp
+++ Python/If-Else.cpp
@@ -11,7 +11,12 @@ import sys
 if __name__ == '__main__':
     n = int(raw_input().strip())
 
-    if (n % 2) == 0:
-        print ("Not Weird")
+    if (n % 2)== 0:
+
+        if (6 <= n <= 20):
+            print ("Weird")
+        else:
+            print ("Not Weird")
+
     else:
         print ("Weird")
